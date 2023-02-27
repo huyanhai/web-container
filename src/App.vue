f4e210851538c1f3b724c9c8a1b0236e5d13379a
Huyanhai <810839700@qq.com>
2023-02-27 19:52:02 +0800

    首次提交

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import { WebContainerTerminal } from "./utils/webContainer";
+
+onMounted(() => {
+  const contaier = new WebContainerTerminal({
+    terminalEl: "#terminal",
+    editorEl: ".editor",
+    iframeEl: ".preview iframe",
+  });
+});
+</script>
+
+<template>
+  <div class="container">
+    <textarea class="editor"></textarea>
+    <div class="preview">
+      <iframe frameborder="0"></iframe>
+    </div>
+  </div>
+  <div id="terminal"></div>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  border: 0;
+}
+.container {
+  display: flex;
+}
+
+.editor {
+  width: 50%;
+  background: black;
+  height: 200px;
+  color: white;
+}
+.preview {
+  width: 50%;
+  height: 200px;
+  border: 1px solid red;
+  box-sizing: border-box;
+}
+.preview iframe {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/utils/webContainer.ts
@@ -0,0 +1,116 @@
+import { WebContainer } from "@webcontainer/api";
+import { Terminal } from "xterm";
+import "xterm/css/xterm.css";
+
+import { files } from "./files";
+
+export class WebContainerTerminal {
+  private terminal!: Terminal;
+  private webcontainerInstance!: WebContainer;
+  options!: { terminalEl: HTMLElement | string; editorEl: HTMLTextAreaElement | string; iframeEl: HTMLIFrameElement | string };
+  constructor(options: WebContainerTerminal["options"]) {
+    this.options = Object.assign({}, options);
+    this.initContainer();
+    this.initTerminal();
+    // return this;
+  }
+
+  /**
+   * 初始化web容器
+   */
+  async initContainer() {
+    let { editorEl } = this.options;
+    editorEl = typeof editorEl === "string" ? (document.querySelector(editorEl) as HTMLTextAreaElement) : editorEl;
+    window.addEventListener("load", async () => {
+      this.webcontainerInstance = await WebContainer.boot();
+      this.webcontainerInstance.mount(files);
+      (editorEl as HTMLTextAreaElement).value = await this.webcontainerInstance.fs.readFile("package.json", "utf-8");
+
+      const exitCode = await this.installDependencies();
+      if (exitCode !== 0) {
+        throw new Error("Installation failed");
+      }
+      this.startDevServe();
+      this.startShell();
+      this.writeFile();
+    });
+  }
+
+  /**
+   * 初始化终端
+   */
+  initTerminal() {
+    let { terminalEl } = this.options;
+    terminalEl = typeof terminalEl === "string" ? (document.querySelector(terminalEl) as HTMLElement) : terminalEl;
+    if (!terminalEl) {
+      return console.error("元素节点不存在");
+    }
+    try {
+      this.terminal = new Terminal({
+        convertEol: true,
+      });
+      this.terminal.open(terminalEl as HTMLElement);
+    } catch (error) {
+      console.error("初始化终端错误", error);
+    }
+  }
+
+  /**
+   * 安装依赖
+   */
+  async installDependencies() {
+    const _this = this;
+    const installProcess = await this.webcontainerInstance.spawn("npm", ["install"]);
+    installProcess.output.pipeTo(
+      new WritableStream({
+        write(data) {
+          _this.terminal.write(data);
+        },
+      })
+    );
+    return installProcess.exit;
+  }
+
+  async startDevServe() {
+    const _this = this;
+
+    const serverProcess = await this.webcontainerInstance.spawn("npm", ["run", "start"]);
+    serverProcess.output.pipeTo(
+      new WritableStream({
+        write(data) {
+          _this.terminal.write(data);
+        },
+      })
+    );
+    this.webcontainerInstance.on("server-ready", (port, url) => {
+      let { iframeEl } = this.options;
+      iframeEl = typeof iframeEl === "string" ? (document.querySelector(iframeEl) as HTMLIFrameElement) : iframeEl;
+      iframeEl.src = url;
+    });
+  }
+
+  async startShell() {
+    const _this = this;
+    const shellProcess = await this.webcontainerInstance.spawn("jsh");
+    shellProcess.output.pipeTo(
+      new WritableStream({
+        write(data) {
+          _this.terminal.write(data);
+        },
+      })
+    );
+    const writer = shellProcess.input.getWriter();
+    this.terminal.onData((data) => {
+      writer.write(data);
+    });
+  }
+
+  writeFile() {
+    let { editorEl } = this.options;
+    editorEl = typeof editorEl === "string" ? (document.querySelector(editorEl) as HTMLTextAreaElement) : editorEl;
+
+    (editorEl as HTMLTextAreaElement).addEventListener("input", (e) => {
+      console.log(e);
+    });
+  }
+}
